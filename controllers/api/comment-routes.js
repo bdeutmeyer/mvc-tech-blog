dc13db2d4842ff96521e93748775ee79d380f5f7
beth deutmeyer <bethdeutmeyer@gmail.com>
2023-10-28 06:52:46 -0500

    all comment and post routes working

---
+++ controllers/api/comment-routes.js
@@ -0,0 +1,41 @@
+const router = require('express').Router();
+const { Post, Comment } = require('../../models');
+
+// Route to update a comment
+router.put('/:id', async (req, res) => {
+  try {
+    const post = await Comment.update(
+      {
+        content: req.body.content
+      },
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
+    );
+    res.status(200).json(post);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Delete a comment
+router.delete('/:id', async (req, res) => {
+    try {
+      const deleteComment = await Comment.destroy({
+        where: {
+          id: req.params.id,
+        },
+      });
+      if (!deleteComment) {
+        res.status(404).json({ message: 'No comment with this id!' });
+        return;
+      }
+      res.status(200).json(deleteComment);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+module.exports = router;
--- controllers/home-route.js
+++ controllers/home-route.js
@@ -7,7 +7,8 @@ router.get('/', async (req, res) => {
         const postData = await Post.findAll();
         const posts = postData.map((post) => post.get({ plain: true }));
         // Change file name as needed later!
-        res.render('all', { posts });
+        res.status(200).json(postData);
+        // res.render('all', { posts });
     } catch(err) { 
         res.status(500).json(err);
       }
