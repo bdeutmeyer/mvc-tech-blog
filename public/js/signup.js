ddab05ed6ea91dd051ca99d466c1bf158b293404
beth deutmeyer <bethdeutmeyer@gmail.com>
2023-11-09 15:39:57 -0600

    tweaked spacing/comment box size, uncommented reroute line for signup page

--- public/js/signup.js
+++ public/js/signup.js
@@ -13,7 +13,7 @@ const signupFormHandler = async (event) => {
       });
   
       if (response.ok) {
-        // document.location.replace('/');
+        document.location.replace('/');
         console.log('Signed up')
       } else {
         alert('Failed to sign up.');
